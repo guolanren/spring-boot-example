37c391210f121134ee7e7b6649cc615c8c3c1947
guolanren <lanrenguo@gmail.com>
2019-11-07 20:35:57 +0800

    webflux模块

--- boot-starter/src/main/java/name/guolanren/autoconfigure/BootStarterAutoConfiguration.java
+++ boot-starter/src/main/java/name/guolanren/autoconfigure/BootStarterAutoConfiguration.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author guolanren
  */
 @Configuration
 @EnableConfigurationProperties(BootStarterProperties.class)
+@Import({})
 public class BootStarterAutoConfiguration {
 
     @Autowired
---
+++ webflux/src/main/java/name/guolanren/webflux/WebfluxApplication.java
@@ -0,0 +1,39 @@
+package name.guolanren.webflux;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+/**
+ * @author guolanren
+ */
+@SpringBootApplication
+public class WebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxApplication.class, args);
+    }
+
+    @EventListener
+    public void onWebServerReady(WebServerInitializedEvent event) {
+        System.out.println("当前 WebServer 实现类为: xm" + event.getWebServer().getClass().getName());
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> hello() {
+        return route(GET("/hello"),
+                request -> ok().body(Mono.just("Hello Webflux"), String.class));
+    }
+
+}
