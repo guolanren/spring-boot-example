4879d16023bb2f5f6d27d81aedc9e9c2e17794d8
郭耀展 <lanrenguo@gmail.com>
2021-04-14 15:19:36 +0800

    rocketmq模块: 报错处理

--- rocketmq/rocketmq-consumer/src/main/java/name/guolanren/RocketMQConsumerApplication.java
+++ rocketmq/rocketmq-consumer/src/main/java/name/guolanren/RocketMQConsumerApplication.java
@@ -1,8 +1,5 @@
 package name.guolanren;
 
-import name.guolanren.service.OrderPaidService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,21 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author guolanren
  */
 @SpringBootApplication
-public class RocketMQConsumerApplication implements CommandLineRunner {
-
-	@Autowired
-	private OrderPaidService orderPaidService;
+public class RocketMQConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RocketMQConsumerApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-//			orderPaidService.springMessageSend();
-//			orderPaidService.springMessageConvertAndSend();
-//			orderPaidService.syncSend();
-//			orderPaidService.asyncSend();
-//			orderPaidService.syncSendOrderly();
 	}
 }
